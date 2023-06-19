1012d1e837402358aa4d69f4c0f07ede0ea82b1f
Natael Pontarolo <nataelpontarolo@gmail.com>
2023-06-18 21:31:45 -0300

    saving file

--- server.c
+++ server.c
@@ -9,11 +9,7 @@ int main(int argc, char *argv[]) {
     char path[1024];
     getcwd(path, sizeof(path)); // stores the current directory path
     FILE *file = fopen("received_messages.txt", "a");
-    if (file == NULL) {
-          printf("Error opening file.\n");
-          return 1;
-      }
-    
+
     while (1) {
         mensagem_t receivedMsg;
         struct sockaddr_ll addr;
@@ -23,19 +19,32 @@ int main(int argc, char *argv[]) {
 
         if (bytesReceived == sizeof(mensagem_t)) {
             // Message received
+            if (file == NULL) {
+                printf("Error opening file.\n");
+                return 1;
+            }
             // printf("Received message:\n");
             // printf("  ini: %u\n", receivedMsg.ini);
             // printf("  tam: %u\n", receivedMsg.tam);
             // printf("  sequencia: %u\n", receivedMsg.sequencia);
             // printf("  tipo: %u\n", receivedMsg.tipo);
-            printf("%s\n", receivedMsg.dados);
-            if (fprintf(file, "%s\n", receivedMsg.dados) < 0) {
-                printf("Error writing to file.\n");
-                // Handle the error accordingly (e.g., close the file, exit the program)
+            switch (receivedMsg.tipo){
+            case 0x00:
+                printf("  tipo: %u\n", receivedMsg.tipo);
+                break;
+            case 0x01:
+                printf("  tipo: %u\n", receivedMsg.tipo);
+                break;
+            case 0x02:
+                printf("  tipo: %u\n", receivedMsg.tipo);
+                break;
             }
-            fprintf(file, "\n");
+            printf("%s\n", receivedMsg.dados);
+            fprintf(file, "%s\n", receivedMsg.dados);
+            fflush(file);
             // printf("  paridade: %u\n", receivedMsg.paridade);
         }
     }
+    fclose(file); // Close the file before exiting
     return 0;
 }
